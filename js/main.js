3e8434c8dd0a5cd29bebc647cb8275b5a716e2a6
Lokteeva <swsw06@mail.ru>
2023-09-17 17:14:34 +0300

    процесс адаптива 1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
+//Slider
 const owl = $('.owl-carousel');
 owl.owlCarousel({
-  center:true,
-  loop:true,
-  margin: 30,
+  center: true,
+  loop: true,
   startPosition: 1,
   items: 3,
+  margin: 30,  
 });
-$('.slider__btn--next').click(function() {
-    owl.trigger('next.owl.carousel');
-})
-$('.slider__btn--pref').click(function() {
-    
-    owl.trigger('prev.owl.carousel');
-})
+$('.slider__btn--next').click(function () {
+  owl.trigger('next.owl.carousel');
+});
+$('.slider__btn--pref').click(function () {
+  owl.trigger('prev.owl.carousel');
+});
+
+//burgerMenu
+const navBar = document.querySelector('.nav__toggle');
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+
+navBar.onclick = function () {
+  nav.classList.toggle('nav--mobile');
+  menuIcon.classList.toggle('menu-icon-active');
+};
